f220f02962971d6ee8c51c9a7b32ed34f9727222
alexisbenavidez99 <callmealexis49@gmail.com>
2023-12-05 13:43:20 -0600

    adjusting nav/hero section to figma design

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,6 @@ const Footer = props => {
         <div className="flex items-end w-full bg-gray-300">
 
                 <div className="container flex-col flex px-5 py-2 mx-auto md:items-center lg:items-start md:flex-no-wrap">
-                    {/* <div className="flex-shrink-0 w-64 mx-auto text-center md:mx-0 md:text-left"></div> */}
                     <h2 className="text-sm font-medium tracking-widest text-gray-900 uppercase title-font">{name}</h2>
                     <nav className="mb-10 list-none">
                         {items.map(item => {
